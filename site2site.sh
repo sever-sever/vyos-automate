e40f3b86ecd832cda8ffa763323b67b53fa9880f
sever-sever <v.gletenko@vyos.io>
2020-08-21 14:38:24 +0300

    Site2Site generate config

--- site2site.sh
+++ site2site.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+# Generate site2site ike1 configuration for local and remote peer
+#
+
 ESP_GRP="office-srv-esp"
 IKE_GRP="office-srv-ike"
 SECRET="SSSeeccRetT"
-SOURCE_IP="203.0.113.1"
-PEER_IP="203.0.113.2"
-LOCAL_PREFIX="10.1.1.0/24"
-REMOTE_PREFIX="192.168.1.0/24"
+SOURCE_IP="192.168.0.1"
+PEER_IP="192.168.0.2"
+LOCAL_PREFIX="10.111.111.0/24"
+REMOTE_PREFIX="10.222.222.0/24"
 IPSEC_IFACE="eth0"
 
 echo
@@ -23,11 +26,11 @@ set vpn ipsec ike-group $IKE_GRP key-exchange 'ikev1'
 set vpn ipsec ike-group $IKE_GRP lifetime '3600'
 set vpn ipsec ike-group $IKE_GRP proposal 1 encryption 'aes256'
 set vpn ipsec ike-group $IKE_GRP proposal 1 hash 'sha1'
-set vpn ipsec ipsec-interfaces interface $IPEC_IFACE
+set vpn ipsec ipsec-interfaces interface $IPSEC_IFACE
 set vpn ipsec site-to-site peer $PEER_IP authentication mode 'pre-shared-secret'
 set vpn ipsec site-to-site peer $PEER_IP authentication pre-shared-secret '$SECRET'
 set vpn ipsec site-to-site peer $PEER_IP ike-group '$IKE_GRP'
-set vpn ipsec site-to-site peer $PEER_IP local-address '198.51.100.3'
+set vpn ipsec site-to-site peer $PEER_IP local-address $SOURCE_IP
 set vpn ipsec site-to-site peer $PEER_IP tunnel 0 allow-nat-networks 'disable'
 set vpn ipsec site-to-site peer $PEER_IP tunnel 0 allow-public-networks 'disable'
 set vpn ipsec site-to-site peer $PEER_IP tunnel 0 esp-group '$ESP_GRP'
@@ -51,7 +54,7 @@ set vpn ipsec ipsec-interfaces interface $IPSEC_IFACE
 set vpn ipsec site-to-site peer $SOURCE_IP authentication mode 'pre-shared-secret'
 set vpn ipsec site-to-site peer $SOURCE_IP authentication pre-shared-secret '$SECRET'
 set vpn ipsec site-to-site peer $SOURCE_IP ike-group '$IKE_GRP'
-set vpn ipsec site-to-site peer $SOURCE_IP local-address '198.51.100.3'
+set vpn ipsec site-to-site peer $SOURCE_IP local-address $PEER_IP
 set vpn ipsec site-to-site peer $SOURCE_IP tunnel 0 allow-nat-networks 'disable'
 set vpn ipsec site-to-site peer $SOURCE_IP tunnel 0 allow-public-networks 'disable'
 set vpn ipsec site-to-site peer $SOURCE_IP tunnel 0 esp-group '$ESP_GRP'
@@ -59,7 +62,3 @@ set vpn ipsec site-to-site peer $SOURCE_IP tunnel 0 local prefix $REMOTE_PREFIX
 set vpn ipsec site-to-site peer $SOURCE_IP tunnel 0 remote prefix $LOCAL_PREFIX
 
 "
-
-# Config R1
-
-# Config R2
