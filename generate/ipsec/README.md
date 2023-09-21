e5764dd124c1fcaf259d945350a665383a05b108
Viacheslav Hletenko <v.gletenko@vyos.io>
2023-09-21 21:47:29 +0300

    Add generate IPSec configuration

---
+++ generate/ipsec/README.md
@@ -0,0 +1,17 @@
+Usage
+```
+$./ipsec.py
+
+# LEFT CONFIG 1.5
+set vpn ipsec authentication psk PSK id 192.0.2.1
+set vpn ipsec authentication psk PSK id 192.0.2.2
+set vpn ipsec authentication psk PSK secret '1234567890'
+...
+
+# RIGHT CONFIG 1.4
+set vpn ipsec authentication psk PSK id 192.0.2.1
+set vpn ipsec authentication psk PSK id 192.0.2.2
+set vpn ipsec authentication psk PSK secret '1234567890'
+...
+```
+
---
+++ generate/ipsec/ipsec.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Script: vyos_firewall.py
+Author: Viacheslav Hletenko
+Date: 2023
+Description:
+Generate IPSec VyOS configuration. Variables are defined in config.yaml
+
+Usage: python3 ipsec.py > vyos_ipsec.conf
+"""
+
+import yaml
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
+from jinja2 import Template
+
+
+def generate_config(config: dict, template: Template) -> str:
+    """
+    Generate VyOS config rules based on the dictionary configuration.
+
+    Args:
+        config (dict): A dictionary generated from config.yaml.
+        template (jinja2.Template): A Jinja2 template used for generating config.
+
+    Returns:
+        str: The generated config as a string.
+    """
+    return template.render(config)
+
+
+if __name__ == "__main__":
+    config = yaml.load(open('config.yaml'), Loader=yaml.FullLoader)
+    env = Environment(loader=FileSystemLoader('templates'), trim_blocks=True)
+
+    ipsec = env.get_template('ipsec.j2')
+    print(generate_config(config, ipsec))
+
