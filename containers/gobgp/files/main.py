f0fb6e9d060060d48e7da03e87f5cb6ccc7489e1
Viacheslav Hletenko <v.gletenko@vyos.io>
2024-01-26 18:02:42 +0200

    Add opcode_to_operator_map and threads

--- containers/gobgp/files/main.py
+++ containers/gobgp/files/main.py
@@ -2,6 +2,7 @@
 
 import subprocess
 import json
+from concurrent.futures import ThreadPoolExecutor
 
 # Command to execute
 command = "gobgp monitor global rib --json"
@@ -43,6 +44,22 @@ def type_to_match_filter_map(match_type: int, output_format: str = 'formatted') 
     return match_types[match_type][output_format]
 
 
+def opcode_to_operator_map(opcode: int, output_format: str = 'op') -> str:
+    """Maps BGP FlowSpec opcode to operator or nftables operator
+    https://datatracker.ietf.org/doc/html/draft-ietf-idr-rfc5575bis-06#section-4.2.3
+    """
+    opcodes = {
+        129: {'op': '==', 'nft': '=='},
+        131: {'op': '>=', 'nft': '>='},
+        139: {'op': '&&', 'nft': '&&'},
+        145: {'op': '=', 'nft': '=='},
+        146: {'op': '>', 'nft': '>'},
+        148: {'op': '<', 'nft': '<'},
+        149: {'op': '<=', 'nft': '<='},
+    }
+    return opcodes[opcode][output_format]
+
+
 def parse_bgp_update(data):
     """Parse the BGP update message and generate nftables rules"""
     data = json.loads(data)
@@ -50,34 +67,61 @@ def parse_bgp_update(data):
     nft_cmd = ''
     for entry in data:
         print(entry)
+        # Check if 'afi' and 'safi' are present in the attributes
+        # Interested only afi 'IPv4 unicast', safi 'IPv4 flow-spec'
+        afi = ''
+        safi = ''
+        for attr in entry.get('attrs', []):
+            if 'afi' in attr and 'safi' in attr:
+                afi = attr['afi']
+                safi = attr['safi']
+        if not (afi == 1 and safi == 133):
+            continue
+
         nlri = entry.get('nlri', {}).get('value')
         for rule in nlri:
             rule_type = rule.get('type')
             match_filter = type_to_match_filter_map(rule_type)
             match_filter_nft = type_to_match_filter_map(rule_type, output_format='nft')
+
+            # Packet length find op and value
+            op_packet_length = ''
+            if match_filter == 'packet_length':
+                op_packet_length = opcode_to_operator_map(
+                    rule.get('value')[0].get('op'), output_format='nft')
+                print(f'{match_filter} op: {op_packet_length}')
+
             match_values = rule.get('value')
             if isinstance(match_values, dict):
                 if 'prefix' in match_values:
-                    print('-' * 20)
                     prefix = match_values['prefix']
                     print(f'{match_filter}: {prefix}')
                     nft_cmd += f'{match_filter_nft} {prefix} '
             elif isinstance(match_values, list):
                 print(match_filter, ':', match_values[0].get('value'))
-                nft_cmd += f' {match_filter_nft} {match_values[0].get("value")}'
+                nft_cmd += f' {match_filter_nft} {op_packet_length} {match_values[0].get("value")}'
 
     print('-' * 20)
     print(f'nft add rule inet filter input {nft_cmd} counter {action}')
 
 
-# Read and process the command output line by line
-for line in iter(process.stdout.readline, b''):
-    # Decode the JSON data
-    json_data = line.decode("utf-8").strip()
-
-    # Process the data
+def process_bgp_update(json_data):
     parse_bgp_update(json_data)
-    print('-' * 20)
+    print('-' * 20, 'process_bgp_update')
 
-# Wait for the command to finish
-process.wait()
+
+if __name__ == '__main__':
+    # Read and process the command output line by line
+    # Adjust max_workers as needed
+    with ThreadPoolExecutor(max_workers=5) as executor:
+        for line in iter(process.stdout.readline, b''):
+            print('DEBUG ORIGINAL data:', line)
+            # Decode the JSON data
+            json_data = line.decode("utf-8").strip()
+            #print('DEBUG ORIGINAL data:', json_data)
+
+            # Submit the task for processing
+            executor.submit(process_bgp_update, json_data)
+
+    # Wait for the command to finish
+    process.wait()
